96a9c2ba8e532e13257e7ea47a80a568c033b943
abdallah <Abdallah@local.com>
2024-10-08 20:44:57 +0300

    abdallah

--- app.py
+++ app.py
@@ -79,5 +79,5 @@ def pred():
 # Run the app
 if __name__ == '__main__':
     # Get the port from the environment variable or default to 5000
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    
+    app.run(host='0.0.0.0', port=5002)
